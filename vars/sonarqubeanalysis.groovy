62f527ca06621790e8aa64b7bc691354edcb563b
zaidkhan132 <zaidbinazeez435@gmail.com>
2024-12-01 00:57:40 +0530

    Update sonarqubeanalysis.groovy

--- vars/sonarqubeanalysis.groovy
+++ vars/sonarqubeanalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-server -Dsonar.projectName=YouTube-app -Dsonar.projectkey=YouTube-app '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=YouTube-app -Dsonar.projectkey=YouTube-app '''
     }
 }
