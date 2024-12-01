96026facd93d6881a906c9fdcd57d15c4dbd6f1f
zaidkhan132 <zaidbinazeez435@gmail.com>
2024-12-01 19:47:13 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,5 @@
 def call(String dockerHubUsername, String imageName) {
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=8f7b217d97mshcf8792a771e4f09p1e5c98jsn4b4859d3a21d"
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=8f7b217d97mshcf8792a771e4f09p1e5c98jsn4b4859d3a21d -t youtube ."
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     withDockerRegistry([url: 'https:/index.docker.io/v1/', credentialsId: 'docker'])
     sh "docker push ${dockerHubUsername}/${imageName}:latest"
